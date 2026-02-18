c481010752c82af33810878931e15361e9e31db2
GeraltHorif <geralthorif@gmail.com>
2026-02-18 21:38:27 +0100

    add lesson 2_1

---
+++ Classwork/Lesson_2/Lesson 2_1/main.js
@@ -0,0 +1,52 @@
+// let arr;
+// arr = [123, 234,345, -123,0, true, 'asdfhjk'];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[4]);
+// console.log(arr.length);
+//
+// arr[0] = 'new value';
+// console.log(arr);
+// arr[7] = 'okten';
+// console.log(arr);
+// arr[arr.length] = 1293883;
+// console.log(arr);
+// arr[arr.length] = 'hgfgfdiytd';
+// console.log(arr);
+//
+
+// let arr = [
+//     [11,22,33], //0
+//     [],
+//     [],
+// ];
+// console.log(arr);
+// //  0 1 2
+// let innerArray = arr[0];// 11 22 33
+// console.log(innerArray[1]); //22
+// console.log(arr[0][1]);
+
+// Object
+// Old
+
+// let user1id = 1;
+// let user1Name = "Petro";
+// let user1Age = 31;
+
+let user1 = {
+    id: 1,
+    name: "Petro",
+    age: 31,
+    skillsls: ['html', 'js','java'],
+    wife: {
+        name: 'Olga',
+        age: 30
+    }
+}
+// console.log(user1);
+// console.log(user1. id);
+// console.log(user1['age']);
+// console.log(user1.skils[2]);
+
+console.log(user1.wife.name)
