15c0b83a52fd4020a72b876159a07c04010445e0
GeraltHorif <geralthorif@gmail.com>
2026-02-22 19:57:38 +0100

    add lesson 2 full

---
+++ Classwork/Lesson_2/Lesson 2_2/main.js
@@ -0,0 +1,81 @@
+// let  users = [
+//     {skills : ['html','msql','mongo'],id:1,name:'vasyl',age:31,status:true},
+//     {skalls : ['html','js'],id:2,name:'petro',age:33,status:true},
+//     {skalls : ['html','js'],id:3,name:'hanna',age:29,status:true},
+//     {skalls : ['html','msql','mongo'],imsqld:4,name:'olha',age:40,status:true}
+// ];
+// console.log(users);
+// let user0 = users[0];
+//
+// console.log(user0.age);
+// console.log(user0['age']);
+//
+// console.log(users[0].skills[2]);
+// let user = users[0];
+// let skills = user['skills'];
+// console.log(skills[2]);
+
+
+// let user = {
+//     id: 1,
+//     name: 'Petro',
+// };
+// user.age = 31;
+// user['status'] = false;
+// console.log(user);
+//
+// delete user.id;
+// console.log(user);
+
+// let a = 100;
+// let b = a;
+// b = b + 10 ;
+// console.log(a);
+//
+// let user = {name:'jevgen'};
+// let user2 = user;
+// console.log(user2);
+// user2.age = 31;
+// console.log(user2);
+// console.log(user);
+
+// let color = 'green'
+// if (color === 'red') {
+//     console.log('stop');
+// } else {
+//     console.log('go!');
+// }
+
+// let age = +prompt('enter your age');
+// if (age >= 18){
+//     console.log('adult');
+// }else {
+//     console.log('cortoon');
+// }
+
+// let color = prompt('enter color!');
+// if (color === 'green'){
+//     if (confirm('is route clear?')) {
+//         console.log('go!');
+//     }else {
+//         console.log('just wait');
+//     }
+// }else if (color === 'yellow') {
+//     console.log('wait');
+// }else if (color === 'red'){
+//     console.log('stop');
+// }else {
+//     console.log('??????');
+// }
+
+// let color = prompt('enter color!');
+// let isRoadClear = confirm('is route clear?');
+// if (color === 'green' && isRoadClear){
+//     console.log('Go!');
+// }else if (color === 'yellow') {
+//     console.log('wait');
+// }else if (color === 'red'){
+//     console.log('stop');
+// }else {
+//     console.log('??????');
+// }
---
+++ Classwork/Lesson_2/Lwsson 2_3/main.js
@@ -0,0 +1,41 @@
+// let color = prompt('Entre color')
+//
+// switch (color) {
+//     case 'green':
+//     console.log('Go!');
+//     break;
+//     case 'yellow':
+//     console.log('Wait!');
+//     break;
+//     case 'red':
+//         console.log('Stop!');
+//         break;
+//     default:
+//     console.log('???????');
+// }
+
+
+// let obj = {
+//     id: 1,
+//     name: 'javgen',
+//     age: 16
+// }
+//
+// let access;
+// if (obj.age > 18){
+//     access = true;
+// }else {
+//     access = false;
+// }
+// console.log(access);
+
+
+let obj = {
+    id: 1,
+    name: 'javgen',
+    age: 20
+}
+
+let access = obj.age > 18? 'Yes' : 'No';
+
+console.log(access);
