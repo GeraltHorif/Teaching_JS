45294e69f5001e70f3fb8d2a03a65603c30b9cd4
GeraltHorif <geralthorif@gmail.com>
2026-03-30 22:45:38 +0200

    add HW6 > task5 edited

--- Homework/HW_6/Task5/main.js
+++ Homework/HW_6/Task5/main.js
@@ -19,3 +19,40 @@ for (const suit of suits) {
 }
 
 console.log('Усього карт у колоді:', deck.length);
+
+const spadeAce = deck.find(card => card.cardSuit === 'spade' && card.value === 'ace');
+console.log('Піковий туз:', spadeAce);
+
+const allSixes = deck.filter(card => card.value === '6');
+console.log('Усі шістки:', allSixes);
+
+const redCards = deck.filter(card => card.color === 'red');
+console.log('Червоні карти:', redCards);
+
+const diamonds = deck.filter(card => card.cardSuit === 'diamond');
+console.log('Буби:', diamonds);
+const heavyClubs = deck.filter(card => {
+    return card.cardSuit === 'clubs' &&
+        card.value !== '6' &&
+        card.value !== '7' &&
+        card.value !== '8';
+});
+console.log('Трефи від 9 і вище:', heavyClubs);
+
+const sortedDeck = deck.reduce((acc, card) => {
+
+    if (card.cardSuit === 'spade') {
+        acc.spades.push(card);
+    } else if (card.cardSuit === 'diamond') {
+        acc.diamonds.push(card);
+    } else if (card.cardSuit === 'heart') {
+        acc.hearts.push(card);
+    } else if (card.cardSuit === 'clubs') {
+        acc.clubs.push(card);
+    }
+
+
+    return acc;
+}, { spades: [], diamonds: [], hearts: [], clubs: [] });
+
+console.log(sortedDeck);
